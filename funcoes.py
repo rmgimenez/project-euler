308fbb29614dddf35c3c34c8db62a8876b4c7d01
rmgimenez <rmgimenez@gmail.com>
2016-05-02 12:29:33 -0300

    criando a função para ver se um número é primo

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,22 @@
 '''
 Funções de ajuda para resolver os problemas
 '''
+
+# retorna true se o número for primo
+def eh_primo(numero):
+	if numero % 2 == 0:
+		return FALSE
+	else:
+		limite = round(numero / 3)
+		i = 3
+		outro_divisor = false
+		while i <= limite:
+			if numero % i == 0:
+				outro_divisor = true
+			i += 2
+		if(outro_divisor):
+			return false
+		else:
+			return true
+
+print(eh_primo(8))
