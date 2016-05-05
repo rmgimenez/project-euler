c96a0cc82823f6e30d8c915159d87b4eae0fa5ff
rmgimenez <rmgimenez@gmail.com>
2016-05-05 20:53:00 -0300

    resolução do problema 10

---
+++ euler010.py
@@ -0,0 +1,28 @@
+'''
+Summation of primes
+Problem 10
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+'''
+
+def is_prime(n):
+	if n == 2 or n == 3: return True
+	if n < 2 or n%2 == 0: return False
+	if n < 9: return True
+	if n%3 == 0: return False
+	r = int(n**0.5)
+	f = 5
+	while f <= r:
+		#print('\t',f)
+		if n%f == 0: return False
+		if n%(f+2) == 0: return False
+		f +=6
+	return True
+
+soma = 0
+for x in range(1, 2000000):
+	if is_prime(x):
+		soma = soma + x
+
+print(soma)
