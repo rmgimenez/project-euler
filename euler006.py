6082771f62a5c4592a73e12f01c5ecc8a407d2c7
rmgimenez <rmgimenez@gmail.com>
2016-05-04 21:44:51 -0300

    resposta do problema 6

---
+++ euler006.py
@@ -0,0 +1,30 @@
+'''
+Sum square difference
+Problem 6
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+25164150
+[Finished in 0.1s]
+'''
+def soma_dos_quadrados(numeros):
+	total = 0
+	for x in range(1, numeros + 1):
+		total = total + (x ** 2)
+	return total
+
+def quadrado_da_soma(numeros):
+	soma = 0
+	for x in range(1, numeros + 1):
+		soma = soma + x
+	return soma ** 2
+
+numero = 100
+print(quadrado_da_soma(numero) - soma_dos_quadrados(numero))
