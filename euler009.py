f2f4e385c7e6a8fd4469c607f72e99fed15711ad
rmgimenez <rmgimenez@gmail.com>
2016-05-05 20:48:47 -0300

    colocado o break e continue para otimizar o algoritimo

--- euler009.py
+++ euler009.py
@@ -19,5 +19,17 @@ for a in range(1,1000):
 		c = valor_c(a, b)
 		if a + b + c == 1000:
 			resposta = a * b * c
+			print("a =", a)
+			print("b =", a)
+			print("c =", a)
+			break
+	else:
+		continue
+	break
 
 print(resposta)
+
+'''
+Ajuda sobre os break e continue que coloquei no código
+http://stackoverflow.com/questions/6346492/how-to-stop-a-for-loop
+'''
