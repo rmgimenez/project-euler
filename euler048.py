4ce133dda441e3305b7e446b86ada4645379df71
rmgimenez <rmgimenez@gmail.com>
2016-05-05 22:14:28 -0300

    resolução do problema 48

---
+++ euler048.py
@@ -0,0 +1,16 @@
+'''
+Self powers
+Problem 48
+The series, 1**1 + 2**2 + 3**3 + ... + 10**10 = 10405071317.
+
+Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000.
+'''
+from time import time
+start = time()
+
+total = 0
+for i in range(1, 1001):
+	total = total + (i**i)
+print(str(total)[-10::])
+
+print("Time: {0} secs".format(time()-start))
