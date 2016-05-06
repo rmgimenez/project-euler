387c5be93b107aa7f243b9ce6cf2ee7acab868c3
rmgimenez <rmgimenez@gmail.com>
2016-05-05 22:20:28 -0300

    resposta problema 20

---
+++ euler020.py
@@ -0,0 +1,27 @@
+'''
+Factorial digit sum
+Problem 20
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+'''
+from time import time
+start = time()
+
+def fatorial( n ):
+   if n <1:   # base case
+       return 1
+   else:
+       return n * fatorial( n - 1 )  # recursive call
+
+resultado_fatorial = fatorial(100)
+resposta = 0
+for numero in str(resultado_fatorial):
+	resposta = resposta + int(numero)
+
+print(resposta)
+
+print("Time: {0} secs".format(time()-start))
