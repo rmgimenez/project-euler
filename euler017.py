a7290e0d14283fc61adbaa06d337129d96b7fa77
rmgimenez <rmgimenez@gmail.com>
2016-05-05 21:45:36 -0300

    lista numeros

--- euler017.py
+++ euler017.py
@@ -32,3 +32,37 @@ def nome_numero(numero):
 
 	elif numero == 10:
 		return 'ten'
+
+'''
+0 - oh, zero, nil, love, nought
+1 - one
+2 - two
+3 - three
+4 - four
+5 - five
+6 - six
+7 - seven
+8 - eight
+9 - nine
+10 - ten
+11 - eleven
+12 - twelve
+13 - thirteen
+14 - fourteen
+15 - fifteen
+16 - sixteen
+17 - seventeen
+18 - eighteen
+19 - nineteen
+20 - twenty
+21 - twenty-one
+30 - thirty
+40 - forty
+50 - fifty
+60 - sixty
+70 - seventy
+80 - eighty
+90 - ninety
+100 - a/one hundred
+1,000 - a/one thousand
+'''
