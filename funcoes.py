98f16955b6d77cc0a6f13fae9f262889af04bd47
rmgimenez <rmgimenez@gmail.com>
2016-05-07 17:52:25 -0300

    comecei o 25 e fiz uma função de fibonacci recursiva nas funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -93,3 +93,8 @@ def fatorial( n ):
        return 1
    else:
        return n * fatorial( n - 1 )  # recursive call
+
+def fib_rec(n):
+	if n==1 or n==2:
+		return 1
+	return fib_rec(n-1) + fib_rec(n-2)
--- testes.py
+++ testes.py
@@ -1,31 +1,5 @@
 # teste
 import funcoes
 
-class Collatz:
-  def __init__(self):
-    self.memory = {}
-
-  def count(self, n):
-    if n in self.memory:
-      return self.memory[n]
-    if n == 1:
-      return 1
-    if (n % 2) == 0:
-      new_count = self.count(n / 2) + 1
-      self.memory[n] = new_count
-      return new_count
-    else:
-      new_count = self.count((3 * n) + 1) + 1
-      self.memory[n] = new_count
-      return new_count
-
-maximum = -1
-maximum_count = -1
-collatz = Collatz()
-for i in range(1, 1000000):
-  count = collatz.count(i)
-  if count > maximum_count:
-    maximum = i
-    maximum_count = count
-
-print('Number:{}, Count:{}'.format(maximum, maximum_count))
+for x in range(1,10):
+	print(x,funcoes.fib_rec(x))
