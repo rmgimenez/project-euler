7ebddca3a33637e9f157b52ab39bec4f2ff59bd8
rmgimenez <rmgimenez@gmail.com>
2016-05-07 17:23:43 -0300

    testes

--- testes.py
+++ testes.py
@@ -1,5 +1,31 @@
 # teste
 import funcoes
 
-teste = 519432**525806
-print("fim")
+class Collatz:
+  def __init__(self):
+    self.memory = {}
+
+  def count(self, n):
+    if n in self.memory:
+      return self.memory[n]
+    if n == 1:
+      return 1
+    if (n % 2) == 0:
+      new_count = self.count(n / 2) + 1
+      self.memory[n] = new_count
+      return new_count
+    else:
+      new_count = self.count((3 * n) + 1) + 1
+      self.memory[n] = new_count
+      return new_count
+
+maximum = -1
+maximum_count = -1
+collatz = Collatz()
+for i in range(1, 1000000):
+  count = collatz.count(i)
+  if count > maximum_count:
+    maximum = i
+    maximum_count = count
+
+print('Number:{}, Count:{}'.format(maximum, maximum_count))
