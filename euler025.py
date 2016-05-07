35f4514f33d81c507c5681b5beab3a82750539c6
rmgimenez <rmgimenez@gmail.com>
2016-05-07 17:55:05 -0300

    tentando melhorar a função 25, ainda muito lenta

--- euler025.py
+++ euler025.py
@@ -23,14 +23,15 @@ The 12th term, F12, is the first term to contain three digits.
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 '''
 import funcoes
-'''
+
 achou = False
 x = 1
 while achou == False:
 	fib = funcoes.fib_rec(x)
-	if len(str(fib)) == 9:
+	if fib > 10**10:
+	#if len(str(fib)) == 9:
 		achou = True
 		#print("Quantidade =",x," | Numero =",fib)
 	print("Quantidade =",x," | Numero =",fib)
 	x += 1
-'''
+
